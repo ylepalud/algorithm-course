f087127fcb795d63b34a595738a75298ae427071
ylepalud <30958216+ylepalud@users.noreply.github.com>
2023-09-28 10:48:01 +0200

    Add documentation to LinearSearch

--- src/main/java/ylp/algorithm/course/algorithmcourse/search/LinearSearch.java
+++ src/main/java/ylp/algorithm/course/algorithmcourse/search/LinearSearch.java
@@ -2,6 +2,11 @@ package ylp.algorithm.course.algorithmcourse.search;
 
 public class LinearSearch {
 
+    /**
+     * Implementation of the linear search
+     *
+     * Complexity: o(n)
+     */
     public static boolean hasItem(int[] array, int value) {
         for (int element: array) {
             if (element == value) return true;
