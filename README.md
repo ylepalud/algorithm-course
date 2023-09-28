1092b75648b5047f7b370c38bbd71535b1121c4c
ylepalud <30958216+ylepalud@users.noreply.github.com>
2023-09-28 08:54:46 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # algorithm-course
+
+## Books
+
+* The introduction to algorithms
+* For programmers Who don't know how to Datastructure and would like to do other things well too
+
---
+++ src/main/java/ylp/algorithm/course/algorithmcourse/AlgorithmCourseApplication.java
@@ -0,0 +1,13 @@
+package ylp.algorithm.course.algorithmcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmCourseApplication.class, args);
+	}
+
+}
